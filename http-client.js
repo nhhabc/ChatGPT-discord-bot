3d2a4300861c49c78003d8c9d6fb3df38b689476
nhhabc <appleluvxx9x@gmail.com>
2023-03-19 12:39:25 +0700

    initial

---
+++ http-client.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const DOMAIN = "https://api.openai.com/v1/chat/completions"
+
+let createRequest = (baseURL) => {
+
+  let timeout = 1000 * 60 * 5;
+  const request = axios.create({
+      baseURL: baseURL,
+      timeout: timeout,
+
+  });
+
+  request.interceptors.response.use(response => {
+      return response.data;
+  }, error => {
+
+      /**
+       * perform redirect to login page when server response with status 401 ( un authorization )
+       *
+       */
+      return Promise.reject(error);
+  });
+
+  request.interceptors.request.use(function (config) {
+
+      /**
+       * add Authorization header to request if user authenticated, run before sent request
+       */
+      config.maxBodyLength = Infinity
+      config.headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+      }
+
+      return config;
+
+  }, function (error) {
+      return Promise.reject(error);
+  });
+
+  return request;
+};
+
+
+export default createRequest(DOMAIN);
---
+++ index.js
@@ -0,0 +1,43 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+import { Configuration, OpenAIApi } from 'openai';
+import dotenv from "dotenv"
+import httpClient from './http-client.js';
+
+dotenv.config()
+
+const prefix = "gpt"
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent], partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+})
+
+client.on("messageCreate", async msg => {
+  if(!msg.content.startsWith(prefix) || msg.author.bot) return
+  try {
+    const args = msg.content.slice(prefix.length).split(/ +/)
+    const command = args.join(" ").toLowerCase()
+
+    const data = {
+      model: "gpt-3.5-turbo",
+      messages: [
+          { "role": "user", "content": command }
+      ],
+      temperature: 0.7
+  };
+
+    const chatGpt = await httpClient.post("", data)
+
+    if (msg.content) {
+      msg.reply({ content: chatGpt.choices[0].message.content, ephemeral: true });
+    }
+  } catch (err) {
+    console.log(err);
+    if (msg.content) {
+      msg.reply({ content: 'ChatGpt is sleeping', ephemeral: true });
+    }
+  }
+});
+
+client.login(process.env.TOKEN)
