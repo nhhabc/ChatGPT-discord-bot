410937c721262e2ff9180e632c95f45bc8c07045
nhhabc <appleluvxx9x@gmail.com>
2023-03-19 16:30:30 +0700

    fixed

--- index.js
+++ index.js
@@ -12,47 +12,45 @@ const prefix = "gpt"
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent], partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}`);
-})
-
-client.on("messageCreate", async msg => {
-  if(!msg.content.startsWith(prefix) || msg.author.bot) return
-  msg.channel.sendTyping()
-  try {
-    const args = msg.content.slice(prefix.length).split(/ +/)
-    const command = args.join(" ").toLowerCase()
-
-    const data = {
-      model: "gpt-3.5-turbo",
-      messages: [
-          { "role": "user", "content": command }
-      ],
-      temperature: 0.7
-  };
-
-    const chatGpt = await httpClient.post("", data)
-
-    if (msg.content) {
-      msg.reply({ content: chatGpt.choices[0].message.content, ephemeral: true });
-    }
-  } catch (err) {
-    console.log(err);
-    if (msg.content) {
-      msg.reply({ content: 'ChatGpt is sleeping', ephemeral: true });
-    }
-  }
-});
-
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
 
 client.login(process.env.TOKEN)
 
-app.listen(5000, () => {
+app.listen(5000, (req, res) => {
   console.log("Running on port 5000.");
+  client.on("ready", () => {
+    res.send(`Logged in as ${client.user.tag}`);
+  })
+  
+  client.on("messageCreate", async msg => {
+    if(!msg.content.startsWith(prefix) || msg.author.bot) return
+    msg.channel.sendTyping()
+    try {
+      const args = msg.content.slice(prefix.length).split(/ +/)
+      const command = args.join(" ").toLowerCase()
+  
+      const data = {
+        model: "gpt-3.5-turbo",
+        messages: [
+            { "role": "user", "content": command }
+        ],
+        temperature: 0.7
+    };
+  
+      const chatGpt = await httpClient.post("", data)
+  
+      if (msg.content) {
+        msg.reply({ content: chatGpt.choices[0].message.content, ephemeral: true });
+      }
+    } catch (err) {
+      console.log(err);
+      if (msg.content) {
+        msg.reply({ content: 'ChatGpt is sleeping', ephemeral: true });
+      }
+    }
+  });
 });
 
-module.exports = client;
 module.exports = app;
